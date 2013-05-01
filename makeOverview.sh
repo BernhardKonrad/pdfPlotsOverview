89ca93aa3a7b1a5d2c66d0d3f78f442a2ba770f2
Bernhard Konrad <bernhard.konrad@gmail.com>
2013-05-01 09:21:47 -0700

    Reads directory name where image files (and temporary tex file) are stored. Final output in pwd

--- makeOverview.sh
+++ makeOverview.sh
@@ -1,26 +1,27 @@
-rm temp.*
-rm overview.pdf
-touch temp.tex
-echo "\documentclass{article}" >> temp.tex
-echo "\usepackage[english]{babel}" >> temp.tex
-echo "\usepackage[margin=2cm]{geometry}" >> temp.tex
-echo "\usepackage{graphicx}" >> temp.tex
-echo "\begin{document}" >> temp.tex
-echo "\begin{center}" >> temp.tex
-for f in *.pdf
+dir=$1
+rm $dir/temp.*
+rm $dir/overview.pdf
+touch $dir/temp.tex
+echo "\documentclass{article}" >> $dir/temp.tex
+echo "\usepackage[english]{babel}" >> $dir/temp.tex
+echo "\usepackage[margin=2cm]{geometry}" >> $dir/temp.tex
+echo "\usepackage{graphicx}" >> $dir/temp.tex
+echo "\begin{document}" >> $dir/temp.tex
+echo "\begin{center}" >> $dir/temp.tex
+for f in $(ls $dir/*.pdf)
 do
-    echo "\includegraphics[height=.45\textheight]{$f}" >> temp.tex
-    echo " " >> temp.tex
-    echo "\bigskip" >> temp.tex
-    echo " " >> temp.tex
+    echo "\includegraphics[height=.45\textheight]{$f}" >> $dir/temp.tex
+    echo " " >> $dir/temp.tex
+    echo "\bigskip" >> $dir/temp.tex
+    echo " " >> $dir/temp.tex
 done
-echo "\end{center}" >> temp.tex
-echo "\end{document}" >> temp.tex
-pdflatex temp.tex
+echo "\end{center}" >> $dir/temp.tex
+echo "\end{document}" >> $dir/temp.tex
+pdflatex $dir/temp.tex
 mv temp.pdf overview.pdf
 rm temp.*
 if [ $(uname -a | grep mac | wc -l) = "1" ]; then
-    open -a Preview overview.pdf
+    open -a Preview $dir/overview.pdf
 elif [ $(uname -a | grep linux | wc -l) = "1" ]; then
-    acroread overview.pdf
+    acroread $dir/overview.pdf
 fi
